de39d2a5c8395b8b66a11146acc4300e52258d14
nakanoi <nakanoinryota@gmail.com>
2021-12-14 23:48:54 +0900

    Change all mailer actions and layout.

--- server/app/jobs/send_recipe_job.rb
+++ server/app/jobs/send_recipe_job.rb
@@ -3,6 +3,8 @@ class SendRecipeJob < ApplicationJob
 
   def perform(*args)
     jobLogger.info "Send Mail on #{Date.today.strftime("%Y-%m-%d")} Started."
-    DailyRecipeMailer.send_recipe_to_all_users.deliver
+    User.all.each do |user|
+      DailyRecipeMailer.send_recipe_to_user(user).deliver_now
+    end
   end
 end
